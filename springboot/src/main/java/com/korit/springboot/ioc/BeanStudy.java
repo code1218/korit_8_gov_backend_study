9df88e236c1298b755132c9528fdd13b276e356a
code1218 <skjil1218@gmail.com>
2025-05-26 17:51:00 +0900

    JPA 기초

---
+++ springboot/src/main/java/com/korit/springboot/ioc/BeanStudy.java
@@ -0,0 +1,7 @@
+package com.korit.springboot.ioc;
+
+import org.springframework.stereotype.Component;
+
+public class BeanStudy {
+
+}
--- springboot/src/main/java/com/korit/springboot/ioc/엘지배터리.java
+++ springboot/src/main/java/com/korit/springboot/ioc/엘지배터리.java
@@ -2,7 +2,6 @@ package com.korit.springboot.ioc;
 
 import org.springframework.stereotype.Component;
 
-@Component
 public class 엘지배터리 implements 배터리 {
     @Override
     public void 전류공급() {
